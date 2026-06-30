64f0bc7344b9cc95edc045d67b114de7fd52250e
Abhigyan <abhi20gyan05@gmail.com>
2026-06-30 18:53:29 +0530

    created basic model

--- server.js
+++ server.js
@@ -5,10 +5,38 @@ const app = require('./app');
 //saves env from our file and saves it for nodeks
 // console.log(process.env);
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
-mongoose.connect(DB).then((con) => {
-  console.log(con.connections);
-});
+mongoose.connect(DB).then(console.log('connection to DB succesfull🚀✅'));
 const port = process.env.PORT || 3000;
+
+//database schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a rating'] },
+});
+//model out of the schema
+const Tour = mongoose.model('Tour', tourSchema);
+
+//creating a basic tour
+const testTour = new Tour({
+  name: 'Pacific crest trail',
+  rating: 4.7,
+  price: 497,
+});
+//adding document to the model
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(port, () => {
   console.log('App running on port 3000');
 });
